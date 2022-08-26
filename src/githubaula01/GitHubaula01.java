30c2b11b777ae9090d7a1ee5b1daf012363e93ca
Fernando Capeletti <fernando.capeletti@alunos.sc.senac.br>
2022-08-26 20:33:14 -0300

    fiz o hello world

---
+++ src/githubaula01/GitHubaula01.java
@@ -0,0 +1,16 @@
+package githubaula01;
+
+/**
+ *
+ * @author fernando.capeletti
+ */
+public class GitHubaula01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+    
+}
