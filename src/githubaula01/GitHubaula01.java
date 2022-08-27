bcfe5d176570f8fcd52319406a9e0a189c787dfc
Fernando Capeletti <fernando.capeletti@alunos.sc.senac.br>
2022-08-26 21:03:55 -0300

    Nova classe carro e nova impressão

--- src/githubaula01/GitHubaula01.java
+++ src/githubaula01/GitHubaula01.java
@@ -11,6 +11,7 @@ public class GitHubaula01 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        System.out.println("Bem vindo ao Git!!");
     }
     
 }
---
+++ src/githubaula01/carro.java
@@ -0,0 +1,14 @@
+package githubaula01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fernando.capeletti
+ */
+public class carro {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+            
+}
